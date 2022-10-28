a64225428b71da252188bd2280d620f1839ac9e8
lucaspericles <lucaspericles.fp@gmail.com>
2022-10-27 21:57:56 -0300

    ajuste nos métodos

---
+++ src/main/java/br/com/acqiogrowup/controlador/ProdutoControlador.java
@@ -0,0 +1,21 @@
+package br.com.acqiogrowup.controlador;
+
+import br.com.acqiogrowup.modelo.Produto;
+import br.com.acqiogrowup.repositorio.ProdutoRepositorio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/produto")
+public class ProdutoControlador {
+
+    @Autowired
+    ProdutoRepositorio produtoRepositorio;
+
+    @GetMapping
+    public List<Produto> listar(){
+        return produtoRepositorio.findAll();
+    }
+}
